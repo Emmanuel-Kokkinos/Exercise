23e3b8e12bc7a9b3948325a57d812feff156fe05
Emmanuel-Kokkinos <Emmanuel.Kokkinos@hotmail.com>
2022-07-09 00:13:20 -0400

    Created POST requests

--- src/main/java/com/mycompany/exercise/exercises/ExerciseService.java
+++ src/main/java/com/mycompany/exercise/exercises/ExerciseService.java
@@ -1,17 +1,18 @@
 package com.mycompany.exercise.exercises;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExerciseService {
-    List<Exercises> exercises = Arrays.asList(
+    private List<Exercises> exercises = new ArrayList<>(Arrays.asList(
         new Exercises("1", "test1"),
         new Exercises("2", "test2"),
         new Exercises("3", "test3"),
         new Exercises("4", "test4")
-    );
+    ));
     
     public List<Exercises> getAllExercises() {
         return exercises;
@@ -19,5 +20,9 @@ public class ExerciseService {
     
     public Exercises getExercise(String id) {
         return exercises.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addExercise(Exercises exercise) {
+        exercises.add(exercise);
     }
 }
--- src/main/java/com/mycompany/exercise/exercises/ExercisesController.java
+++ src/main/java/com/mycompany/exercise/exercises/ExercisesController.java
@@ -3,7 +3,9 @@ package com.mycompany.exercise.exercises;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +22,10 @@ public class ExercisesController {
     @RequestMapping("/exercises/{id}")
     public Exercises getExercise(@PathVariable String id) {
         return exerciseService.getExercise(id);
+    }
+    
+    @RequestMapping(method=RequestMethod.POST, value="/exercises")
+    public void addExercise(@RequestBody Exercises exercise) {
+        exerciseService.addExercise(exercise);
     }
 }
